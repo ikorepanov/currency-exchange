da60dc96c4330dc01f093586d0d45466dd7dfb7e
ikorepanov <ikorepanov@gmail.com>
2026-01-22 18:14:05 +0700

    refactor: add BASE_DIR to create_db

--- src/currency_exchange/db/create_db.py
+++ src/currency_exchange/db/create_db.py
@@ -18,17 +18,26 @@ from currency_exchange.constants import (
 )
 from currency_exchange.utils.data_helpers import round_decimal
 
+BASE_DIR = Path(__file__).resolve().parent
+
+file_path_db = BASE_DIR / 'db.sqlite'
+file_path_currencies = BASE_DIR / 'data' / 'Currencies.csv'
+file_path_exchange_rates = BASE_DIR / 'data' / 'ExchangeRates.csv'
+
 
 def create_db() -> None:
-    with connect('./src/currency_exchange/db/db.sqlite') as conn:
+    with connect(str(file_path_db)) as conn:
         cur = conn.cursor()
         cur.execute(DROP_CURRENCIES_TABLE_SQL)
         cur.execute(CREATE_CURRENCIES_TABLE_SQL)
         cur.execute(CREATE_UNIQUE_INDEX_CURRENCIES_SQL)
 
-        with Path('./src/currency_exchange/db/data/Currencies.csv').open() as handle:
+        with file_path_currencies.open(encoding='utf-8') as handle:
             for line in handle:
-                csv_parts = line.strip().split(',')
+                line = line.strip()
+                if not line:
+                    continue
+                csv_parts = line.split(',')
 
                 code = csv_parts[0]
                 name = csv_parts[1]
@@ -43,9 +52,12 @@ def create_db() -> None:
         cur.execute(CREATE_EXCHANGE_RATES_TABLE_SQL)
         cur.execute(CREATE_UNIQUE_INDEX_EXCHANGE_RATES_SQL)
 
-        with Path('./src/currency_exchange/db/data/ExchangeRates.csv').open() as handle:
+        with file_path_exchange_rates.open(encoding='utf-8') as handle:
             for line in handle:
-                csv_parts = line.strip().split(',')
+                line = line.strip()
+                if not line:
+                    continue
+                csv_parts = line.split(',')
 
                 base_cur_code = csv_parts[0]
                 target_cur_code = csv_parts[1]
